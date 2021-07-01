3c0e19df25e6258fb6c938599df69db58a819619
Brian Warner <warner@lothar.com>
2021-07-01 12:56:06 -0700

    add monitor-slog-block-time.py

---
+++ monitor-slog-block-time.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+import sys, json
+from collections import defaultdict
+
+# run like this:
+#   tail -n 10000 -F chain.slog | python3 monitor-slog-block-time.py
+# produces output like:
+#
+# -- block  swingset (avg2min)    %  --   block (avg2min)
+#    1140      0.666    0.666   15%  --   4.360    4.360
+#    1141      0.522    0.594   12%  --   4.921    4.641
+#    1142      1.369    0.852   16%  --   5.923    5.068
+#    1143      0.704    0.815   16%  --   4.387    4.898
+#    1144      0.608    0.774   15%  --   4.970    4.912
+
+blocks = defaultdict(dict)
+recent_blocks = []
+
+def abbrev(t):
+    return "%1.3f" % t
+def perc(n):
+    return "%3d%%" % (n * 100)
+
+
+head = "-- block  swingset (avg2min)    %  --   block (avg2min)  "
+fmt  = "  %5d     %6s   %6s  %4s  --  %6s   %6s"
+
+class Summary:
+    headline_counter = 0
+    def summarize(self):
+        if self.headline_counter == 0:
+            print(head)
+            self.headline_counter = 20
+        self.headline_counter -= 1
+        ( height, block_time, idle_time, swingset_time, swingset_percentage ) = recent_blocks[-1]
+        # 2 minutes is nominally 120/6= 20 blocks
+        recent = recent_blocks[-20:]
+        avg_block_time = sum(b[1] for b in recent) / len(recent)
+        avg_swingset_time = sum(b[3] for b in recent) / len(recent)
+        avg_swingset_percentage = sum(b[4] for b in recent) / len(recent)
+
+        print(fmt % (height,
+                     abbrev(swingset_time), abbrev(avg_swingset_time),
+                     perc(avg_swingset_percentage),
+                     abbrev(block_time), abbrev(avg_block_time)))
+
+s = Summary()
+
+for line in sys.stdin:
+    data = json.loads(line)
+    if data["type"] in ["cosmic-swingset-begin-block",
+                        "cosmic-swingset-end-block-start",
+                        "cosmic-swingset-end-block-finish"]:
+        t = data["type"][len("cosmic-swingset-"):]
+        height = data["blockHeight"]
+        blocks[height][t] = data["time"]
+        if len(blocks) < 2:
+            continue
+        if t == "end-block-finish":
+            idle_time = None
+            block_time = None
+            swingset_time = blocks[height]["end-block-finish"] - blocks[height]["end-block-start"]
+            if blocks[height-1]:
+                idle_time = blocks[height]["begin-block"] - blocks[height-1]["end-block-finish"]
+                block_time = blocks[height]["end-block-finish"] - blocks[height-1]["end-block-finish"]
+                swingset_percentage = swingset_time / block_time
+            recent_blocks.append([ height, block_time, idle_time, swingset_time, swingset_percentage ])
+            s.summarize()
+
