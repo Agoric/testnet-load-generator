b55b5e9424f2866ee35d3715720fab3ae1f47ed5
Mathieu Hofman <mathieu@agoric.com>
2021-07-02 00:15:33 +0000

    Formatting of local chain operations

--- runner/lib/test-local-chain.js
+++ runner/lib/test-local-chain.js
@@ -60,187 +60,187 @@ export const makeTestOperations = ({
     end: false,
   });
 
-  return harden({
-    setupTest: async ({ stdout, stderr, config = {} }) => {
-      const { console, stdio } = getConsoleAndStdio(
-        'setup-test',
-        stdout,
-        stderr,
+  /** @param {import("./test-operations.js").OperationBaseOption & {config?: {reset?: boolean}}} options */
+  const setupTest = async ({ stdout, stderr, config: { reset } = {} }) => {
+    const { console, stdio } = getConsoleAndStdio('setup-test', stdout, stderr);
+
+    console.log('Starting');
+
+    if (reset) {
+      console.log('Resetting chain node and client state');
+      const stateDir = dirname(chainDirPrefix);
+      await childProcessDone(pipedSpawn('rm', ['-rf', stateDir], { stdio }));
+      await childProcessDone(
+        pipedSpawn('git', ['checkout', '--', stateDir], {
+          stdio,
+        }),
       );
+    }
+    await childProcessDone(pipedSpawn('agoric', ['install'], { stdio }));
 
-      console.log('Starting');
+    console.log('Done');
+  };
 
-      const { reset } = /** @type {{reset?: boolean}} */ (config);
+  /** @param {import("./test-operations.js").OperationBaseOption} options */
+  const runChain = async ({ stdout, stderr, timeout = 30 }) => {
+    const { console, stdio } = getConsoleAndStdio('chain', stdout, stderr);
 
-      if (reset) {
-        console.log('Resetting state');
-        const stateDir = dirname(chainDirPrefix);
-        await childProcessDone(pipedSpawn('rm', ['-rf', stateDir], { stdio }));
-        await childProcessDone(
-          pipedSpawn('git', ['checkout', '--', stateDir], {
-            stdio,
-          }),
+    console.log('Starting chain');
+
+    const slogFifo = await makeFIFO('chain.slog');
+    const slogLines = new LineStreamTransform();
+    const slogPipeResult = pipeline(slogFifo, slogLines);
+
+    const chainEnv = Object.create(process.env);
+    chainEnv.SLOGFILE = slogFifo.path;
+
+    const launcherCp = pipedSpawn(
+      'agoric',
+      ['start', 'local-chain', '--verbose'],
+      { stdio, env: chainEnv, detached: true },
+    );
+
+    let stopped = false;
+
+    // Chain exit with code 98 when killed
+    const chainDone = childProcessDone(launcherCp, {
+      ignoreExitCode: true,
+    }).then((code) => {
+      if (code !== 0 && (!stopped || code !== 98)) {
+        return Promise.reject(
+          new Error(`Chain exited with non-zero code: ${code}`),
         );
       }
-      await childProcessDone(pipedSpawn('agoric', ['install'], { stdio }));
+      return 0;
+    });
 
-      console.log('Done');
-    },
-    runChain: async ({ stdout, stderr, timeout = 30 }) => {
-      const { console, stdio } = getConsoleAndStdio('chain', stdout, stderr);
+    chainDone.then(
+      () => console.log('Chain exited successfully'),
+      (error) => console.error('Chain exited with error', error),
+    );
 
-      console.log('Starting chain');
+    const [chainStarted, firstBlock, outputParsed] = whenStreamSteps(
+      launcherCp.stdout,
+      [
+        { matcher: chainStartRE },
+        { matcher: chainBlockBeginRE, resultIndex: -1 },
+      ],
+      {
+        waitEnd: false,
+      },
+    );
 
-      const slogFifo = await makeFIFO('chain.slog');
-      const slogLines = new LineStreamTransform();
-      const slogPipeResult = pipeline(slogFifo, slogLines);
+    const done = PromiseAllOrErrors([
+      slogPipeResult,
+      outputParsed,
+      chainDone,
+    ]).then(() => {});
 
-      const chainEnv = Object.create(process.env);
-      chainEnv.SLOGFILE = slogFifo.path;
+    return tryTimeout(
+      timeout * 1000,
+      async () => {
+        await chainStarted;
 
-      const launcherCp = pipedSpawn(
-        'agoric',
-        ['start', 'local-chain', '--verbose'],
-        { stdio, env: chainEnv, detached: true },
-      );
+        console.log('Chain running');
 
-      let stopped = false;
-
-      // Chain exit with code 98 when killed
-      const chainDone = childProcessDone(launcherCp, {
-        ignoreExitCode: true,
-      }).then((code) => {
-        if (code !== 0 && (!stopped || code !== 98)) {
-          return Promise.reject(
-            new Error(`Chain exited with non-zero code: ${code}`),
-          );
-        }
-        return 0;
-      });
-
-      chainDone.then(
-        () => console.log('Chain exited successfully'),
-        (error) => console.error('Chain exited with error', error),
-      );
-
-      const [chainStarted, firstBlock, outputParsed] = whenStreamSteps(
-        launcherCp.stdout,
-        [
-          { matcher: chainStartRE },
-          { matcher: chainBlockBeginRE, resultIndex: -1 },
-        ],
-        {
-          waitEnd: false,
-        },
-      );
-
-      const done = PromiseAllOrErrors([
-        slogPipeResult,
-        outputParsed,
-        chainDone,
-      ]).then(() => {});
-
-      return tryTimeout(
-        timeout * 1000,
-        async () => {
-          await chainStarted;
-
-          console.log('Chain running');
-
-          const [storageLocation, processInfo] = await PromiseAllOrErrors([
-            chainStarted.then(findDirByPrefix),
-            getProcessInfo(
-              /** @type {number} */ (launcherCp.pid),
-            ).then((launcherInfo) =>
-              getChildMatchingArgv(launcherInfo, chainArgvMatcher),
-            ),
-          ]);
-
-          const stop = () => {
-            stopped = true;
-            process.kill(processInfo.pid);
-          };
-
-          return harden({
-            stop,
-            done,
-            ready: firstBlock,
-            slogLines: {
-              [Symbol.asyncIterator]: () => slogLines[Symbol.asyncIterator](),
-            },
-            storageLocation,
-            processInfo,
-          });
-        },
-        async () => {
-          // Avoid unhandled rejections for promises that can no longer be handled
-          Promise.allSettled([done, firstBlock]);
-          launcherCp.kill();
-          slogFifo.close();
-        },
-      );
-    },
-    runClient: async ({ stdout, stderr, timeout = 20 }) => {
-      const { console, stdio } = getConsoleAndStdio('client', stdout, stderr);
-
-      console.log('Starting client');
-
-      const launcherCp = pipedSpawn('agoric', ['start', 'local-solo'], {
-        stdio,
-        detached: true,
-      });
-
-      const clientDone = childProcessDone(launcherCp);
-
-      clientDone.then(
-        () => console.log('Client exited successfully'),
-        (error) => console.error('Client exited with error', error),
-      );
-
-      const [clientStarted, walletReady, outputParsed] = whenStreamSteps(
-        launcherCp.stdout,
-        [
-          { matcher: clientStartRE, resultIndex: -1 },
-          { matcher: clientWalletReadyRE, resultIndex: -1 },
-        ],
-        {
-          waitEnd: false,
-        },
-      );
-
-      const done = PromiseAllOrErrors([
-        outputParsed,
-        clientDone,
-      ]).then(() => {});
-
-      return tryTimeout(
-        timeout * 1000,
-        async () => {
-          await clientStarted;
-
-          console.log('Client running');
-
-          const processInfo = await getProcessInfo(
+        const [storageLocation, processInfo] = await PromiseAllOrErrors([
+          chainStarted.then(findDirByPrefix),
+          getProcessInfo(
             /** @type {number} */ (launcherCp.pid),
           ).then((launcherInfo) =>
-            getChildMatchingArgv(launcherInfo, clientArgvMatcher),
-          );
+            getChildMatchingArgv(launcherInfo, chainArgvMatcher),
+          ),
+        ]);
 
-          const stop = () => process.kill(processInfo.pid);
+        const stop = () => {
+          stopped = true;
+          process.kill(processInfo.pid);
+        };
 
-          return harden({
-            stop,
-            done,
-            ready: walletReady,
-          });
-        },
-        async () => {
-          // Avoid unhandled rejections for promises that can no longer be handled
-          Promise.allSettled([done, walletReady]);
-          launcherCp.kill();
-        },
-      );
-    },
+        return harden({
+          stop,
+          done,
+          ready: firstBlock,
+          slogLines: {
+            [Symbol.asyncIterator]: () => slogLines[Symbol.asyncIterator](),
+          },
+          storageLocation,
+          processInfo,
+        });
+      },
+      async () => {
+        // Avoid unhandled rejections for promises that can no longer be handled
+        Promise.allSettled([done, firstBlock]);
+        launcherCp.kill();
+        slogFifo.close();
+      },
+    );
+  };
+
+  /** @param {import("./test-operations.js").OperationBaseOption} options */
+  const runClient = async ({ stdout, stderr, timeout = 20 }) => {
+    const { console, stdio } = getConsoleAndStdio('client', stdout, stderr);
+
+    console.log('Starting client');
+
+    const launcherCp = pipedSpawn('agoric', ['start', 'local-solo'], {
+      stdio,
+      detached: true,
+    });
+
+    const clientDone = childProcessDone(launcherCp);
+
+    clientDone.then(
+      () => console.log('Client exited successfully'),
+      (error) => console.error('Client exited with error', error),
+    );
+
+    const [clientStarted, walletReady, outputParsed] = whenStreamSteps(
+      launcherCp.stdout,
+      [
+        { matcher: clientStartRE, resultIndex: -1 },
+        { matcher: clientWalletReadyRE, resultIndex: -1 },
+      ],
+      {
+        waitEnd: false,
+      },
+    );
+
+    const done = PromiseAllOrErrors([outputParsed, clientDone]).then(() => {});
+
+    return tryTimeout(
+      timeout * 1000,
+      async () => {
+        await clientStarted;
+
+        console.log('Client running');
+
+        const processInfo = await getProcessInfo(
+          /** @type {number} */ (launcherCp.pid),
+        ).then((launcherInfo) =>
+          getChildMatchingArgv(launcherInfo, clientArgvMatcher),
+        );
+
+        const stop = () => process.kill(processInfo.pid);
+
+        return harden({
+          stop,
+          done,
+          ready: walletReady,
+        });
+      },
+      async () => {
+        // Avoid unhandled rejections for promises that can no longer be handled
+        Promise.allSettled([done, walletReady]);
+        launcherCp.kill();
+      },
+    );
+  };
+
+  return harden({
+    setupTest,
+    runChain,
+    runClient,
     runLoadgen: makeLoadgenOperation({ pipedSpawn }),
   });
 };
