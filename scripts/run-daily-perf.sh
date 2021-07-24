c600717db1f24d6ed23217b58b8055949ae73a21
Mathieu Hofman <mathieu@agoric.com>
2021-07-24 03:40:53 +0000

    Fix daily perf script interruptibility The perf script would ignore directed signals while sleeping or waiting for docker, launch those in the background instead and wait on them. Send a term signal to docker instead of stopping to prevent abrupt kill Streamline signal handling logic

--- scripts/run-daily-perf.sh
+++ scripts/run-daily-perf.sh
@@ -4,34 +4,38 @@ set -x
 # Runs a full 24h loadgen on the latest HEAD and save the output in the current directory
 # Requires a docker image named `loadgen-runner`
 
-running=0
 SDK_REPO="${SDK_REPO:-https://github.com/Agoric/agoric-sdk.git}"
+running=1
 DOCKER_ID=
+SLEEP_PID=
 
-stop() { [ $running -eq 0 ] && exit 0 || running=0; }
-stop_container() { [ -z "${DOCKER_ID}" ] || docker stop ${DOCKER_ID}; }
+stop_sleep() { [ -z "$SLEEP_PID" ] && return; kill -TERM $SLEEP_PID; exit 0; }
+stop_container() { [ -z "${DOCKER_ID}" ] && return; docker kill --signal=SIGTERM ${DOCKER_ID}; }
 
 trap '' HUP
-trap 'stop; stop_container' INT TERM
+trap 'running=0; stop_sleep; stop_container' INT TERM
 
-while true
+while [ $running -eq 1 ]
 do
   while true
   do
     SDK_REVISION=$(git ls-remote ${SDK_REPO} HEAD | awk '{ print substr($1,1,12) }')
     OUTPUT_DIR="daily-perf-${SDK_REVISION}"
     [ ! -d "${OUTPUT_DIR}" ] && break
-    sleep 60
+    sleep 60 &
+    SLEEP_PID=$!
+    wait $SLEEP_PID
+    SLEEP_PID=
   done
   echo "processing ${SDK_REVISION}"
   mkdir "${OUTPUT_DIR}"
-  running=1
   DOCKER_ID=$(docker create -v "$(pwd)/${OUTPUT_DIR}:/out" -e SDK_REVISION=${SDK_REVISION} --name "${OUTPUT_DIR}" loadgen-runner --no-reset) || exit $?
   docker start ${DOCKER_ID}
-  docker wait ${DOCKER_ID} >"${OUTPUT_DIR}/exit_code"
+  docker wait ${DOCKER_ID} >"${OUTPUT_DIR}/exit_code" &
+  DOCKER_WAIT_PID=$!
+  while kill -0 $DOCKER_WAIT_PID 2>/dev/null; do wait $DOCKER_WAIT_PID; done
   docker logs ${DOCKER_ID} >"${OUTPUT_DIR}/docker.log" 2>&1
-  [ -d "/var/lib/docker" ] && sudo cat /var/lib/docker/containers/${DOCKER_ID}/${DOCKER_ID}-json.log >"${OUTPUT_DIR}/docker.json.log"
+  [ -d "/var/lib/docker" ] && sudo -n cat /var/lib/docker/containers/${DOCKER_ID}/${DOCKER_ID}-json.log >"${OUTPUT_DIR}/docker.json.log"
   docker rm ${DOCKER_ID}
   DOCKER_ID=
-  stop
 done
