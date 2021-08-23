73826979b743b4bfdbef3ae694e3a4076dadae81
Mathieu Hofman <mathieu@agoric.com>
2021-08-23 19:18:00 +0000

    feat(loadgen): Cloud orchestrator

---
+++ loadgen/firebase/admin/index.js
@@ -0,0 +1,129 @@
+import functions from 'firebase-functions';
+import admin from 'firebase-admin';
+
+import { debounce } from './helpers.js';
+import { updateConfigs, updateNeeded } from './config.js';
+
+admin.initializeApp();
+
+const defaultIncluded = (snap) => snap.exists();
+
+const updateCollectionCount = ({
+  collectionEntryPath,
+  countPath,
+  idParam,
+  logDesc,
+  included = defaultIncluded,
+  excluded = (snap) => !included(snap),
+}) =>
+  functions.database
+    .ref(collectionEntryPath)
+    .onWrite(async (change, context) => {
+      const db = admin.database();
+      const countRef = db.ref(countPath);
+
+      let delta;
+      if (excluded(change.before) && included(change.after)) {
+        delta = 1;
+      } else if (included(change.before) && excluded(change.after)) {
+        delta = -1;
+      } else {
+        return null;
+      }
+
+      await countRef.set(admin.database.ServerValue.increment(delta));
+
+      if (logDesc) {
+        functions.logger.log(
+          `${logDesc} total updated.`,
+          delta > 0 ? `+${delta}` : delta,
+          context.params[idParam],
+        );
+      }
+      return null;
+    });
+
+// Keep track of connecting clients
+export const connectedClientsCount = updateCollectionCount({
+  collectionEntryPath: '/loadgen/clients/{clientId}/connected',
+  countPath: '/loadgen/admin/computed/connectedClients',
+  idParam: 'clientId',
+  logDesc: 'Connected clients',
+  included: (snap) => snap.val() === true,
+  excluded: (snap) => !snap.exists() || snap.val() === false,
+});
+
+// If the number of likes gets deleted, recount the number of likes
+export const recountConnectedClients = functions.database
+  .ref('/loadgen/admin/computed/connectedClients')
+  .onDelete(async (snap) => {
+    const db = admin.database();
+    const countRef = snap.ref;
+    const clientsRef = db.ref('/loadgen/clients');
+    const connectedClients = clientsRef.orderByChild('connected').equalTo(true);
+
+    const connectedClientsSnap = await connectedClients.once('value');
+    return countRef.set(connectedClientsSnap.numChildren());
+  });
+
+// Keep track of active cycles
+export const activeCyclesCount = updateCollectionCount({
+  collectionEntryPath: '/loadgen/cycles/{cycleId}',
+  countPath: '/loadgen/admin/computed/activeCycles',
+  idParam: 'cycleId',
+  logDesc: 'Active cycles',
+  included: (snap) => snap.hasChild('startedAt') && !snap.hasChild('success'),
+});
+
+export const recountActiveCycles = functions.database
+  .ref('/loadgen/admin/computed/activeCycles')
+  .onDelete(async (snap) => {
+    const db = admin.database();
+    const countRef = snap.ref;
+    const cyclesRef = db.ref('/loadgen/cycles');
+    const maybeActiveCycles = cyclesRef.orderByChild('success').equalTo(null);
+
+    const maybeActiveCyclesSnap = await maybeActiveCycles.once('value');
+    const count =
+      maybeActiveCyclesSnap.numChildren() &&
+      Object.values(maybeActiveCyclesSnap.val()).reduce(
+        (acc, cycle) => (cycle.startedAt ? acc + 1 : acc),
+        0,
+      );
+
+    return countRef.set(count);
+  });
+
+// Keep track of requested clients still connected
+export const enrolledClientsCount = updateCollectionCount({
+  collectionEntryPath: '/loadgen/requestedConfigs/{clientId}',
+  countPath: '/loadgen/admin/computed/enrolledClients',
+  idParam: 'clientId',
+  logDesc: 'Enrolled clients',
+});
+
+// If the number of likes gets deleted, recount the number of likes
+export const recountEnrolledClients = functions.database
+  .ref('/loadgen/admin/computed/enrolledClients')
+  .onDelete(async (snap) => {
+    const db = admin.database();
+    const countRef = snap.ref;
+    const configsRef = db.ref('/loadgen/requestedConfigs');
+
+    const enrolledClientsSnap = await configsRef.once('value');
+    return countRef.set(enrolledClientsSnap.numChildren());
+  });
+
+const after = (change) => change.after;
+
+export const adminShapeChange = functions.database
+  .ref('/loadgen/admin')
+  .onWrite(
+    debounce(async (change) => {
+      const data = after(change);
+      if (updateNeeded(data)) {
+        return updateConfigs(data);
+      }
+      return null;
+    }, after),
+  );
