998cfdc1f8b37f57a5972c7498eb72d437d2e692
Turadg Aleahmad <turadg@agoric.com>
2022-04-06 08:56:34 -0700

    fix: add liquidationPenalty to addVaultType

--- loadgen/contract/agent-prepare-loadgen.js
+++ loadgen/contract/agent-prepare-loadgen.js
@@ -393,6 +393,7 @@ export default async function startAgent({
       const rates = {
         debtLimit: AmountMath.make(centralBrand, 1_000_000_000n),
         liquidationMargin: makeRatio(105n, centralBrand),
+        liquidationPenalty: makeRatio(10n, centralBrand, 100n, centralBrand),
         interestRate: makeRatio(250n, centralBrand, BASIS_POINTS),
         loanFee: makeRatio(200n, centralBrand, BASIS_POINTS),
       };
