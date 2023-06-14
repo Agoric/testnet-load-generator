8751cb1505a16bdadb43b50d7dcfcac9d724227a
Mathieu Hofman <mathieu@agoric.com>
2023-06-14 00:37:48 +0000

    fix(loadgen): remove ui dependency from contract

--- loadgen/contract/display.js
+++ loadgen/contract/display.js
@@ -1,4 +1,54 @@
-import { stringifyNat } from '@agoric/ui-components/src/display/natValue/stringifyNat.js';
+// @ts-check
+import { assert, details } from '@agoric/assert';
+
+const CONVENTIONAL_DECIMAL_PLACES = 2;
+
+export const roundToDecimalPlaces = (
+  rightOfDecimalStr = '',
+  decimalPlaces = 0,
+) => {
+  assert.typeof(rightOfDecimalStr, 'string');
+  assert.typeof(decimalPlaces, 'number');
+  assert(
+    decimalPlaces >= 0,
+    details`decimalPlaces must be a number greater or equal to 0`,
+  );
+  // If rightOfDecimalStr isn't long enough, pad with 0s
+  const strPadded = rightOfDecimalStr.padEnd(decimalPlaces, '0');
+  // This is rounding down to the floor
+  // TODO: round more appropriately, maybe bankers' rounding
+  const strRounded = strPadded.substring(0, decimalPlaces);
+  return strRounded;
+};
+
+/**
+ * @param {NatValue | null} natValue
+ * @param {number} [decimalPlaces]
+ * @param {number} [placesToShow]
+ * @returns {string}
+ */
+export const stringifyNat = (
+  natValue = null,
+  decimalPlaces = 0,
+  placesToShow = CONVENTIONAL_DECIMAL_PLACES,
+) => {
+  if (natValue === null) {
+    return '';
+  }
+  assert.typeof(natValue, 'bigint');
+  const str = `${natValue}`.padStart(decimalPlaces, '0');
+  const leftOfDecimalStr = str.substring(0, str.length - decimalPlaces) || '0';
+  const rightOfDecimalStr = roundToDecimalPlaces(
+    `${str.substring(str.length - decimalPlaces)}`,
+    placesToShow,
+  );
+
+  if (rightOfDecimalStr === '') {
+    return leftOfDecimalStr;
+  }
+
+  return `${leftOfDecimalStr}.${rightOfDecimalStr}`;
+};
 
 /**
  * @param {Amount<'nat'>} amount
