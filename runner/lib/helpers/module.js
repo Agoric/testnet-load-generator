66d88c84f590042f9e171c7f6fbd3c529f51716a
usmanmani1122 <usman.sbf@gmail.com>
2025-06-23 08:50:05 +0000

    fix(runner): Use require to resolve config path

--- runner/lib/helpers/module.js
+++ runner/lib/helpers/module.js
@@ -7,21 +7,8 @@ import { pathToFileURL } from 'url';
  * @param {string | URL} parent
  */
 export const resolve = async (specified, parent) => {
-  if (!parent) {
-    throw new TypeError('Invalid parent');
-  }
-  try {
-    if (import.meta.resolve) {
-      return await import.meta.resolve(specified, parent);
-    }
-  } catch (err) {
-    if (/MODULE_NOT_FOUND/.test(/** @type {any} */ (err).code)) {
-      throw err;
-    }
-    // Fall-through
-  }
+  if (!parent) throw new TypeError('Invalid parent');
 
   const require = createRequire(parent);
-
   return pathToFileURL(require.resolve(specified)).href;
 };
